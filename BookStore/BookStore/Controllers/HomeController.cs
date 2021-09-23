8082af952609ba4c3ecac6dae01bb9efaf138373
Shifat Reza <shifatreza44@gmail.com>
2021-09-23 17:42:41 +0600

    34

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,18 +10,30 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string MyProperty { get; set; }
+        [ViewData]
+        public string Title { get; set; }
+
+        [ViewData]
+        public BookModel Book{ get; set; }
         public ViewResult Index()
         {
+            MyProperty = "SHIFAT";
+            Title = "Home";
+            Book = new BookModel() { Title = "System Design", Author = "Rony" };
             return View();
         }
 
         public ViewResult AboutUs()
         {
+            Title = "About Us";
             return View();
         }
 
         public ViewResult ContactUs()
         {
+            Title = "Contact Us";
             return View();
         }
     }
